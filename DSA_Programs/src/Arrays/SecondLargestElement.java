28df0f0608611856f3f6d5ff841b79c7be6c57ac
Jay Bhanushali <jbhanushali70@gmail.com>
2023-05-02 18:15:43 +0530

    Add files via upload

---
+++ DSA_Programs/src/Arrays/SecondLargestElement.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+// Question : Given an array Arr of size N, print second largest distinct element from an array.
+
+// NOTE :: There must be a main method which drives the code and calls print2largest method.
+
+public class SecondLargestElement {
+	int print2largest(int arr[], int n) {
+		if (n > 0) {
+			int first = Integer.MIN_VALUE;
+			int second = Integer.MIN_VALUE;
+			for (int i = 0; i < n; i++) {
+
+				// check for largest element
+				if (arr[i] > first) {
+					second = first;
+					first = arr[i];
+				}
+
+				// check for second largest element and duplicate elements basically if arr[i]>second is true and arr[i]!=first is false means same we have encountered the same element again
+				else if (arr[i] > second && arr[i] != first) {
+					second = arr[i];
+				}
+			}
+
+			// if this condition is true means array contains same elements
+			if (second == Integer.MIN_VALUE) {
+				return -1;
+			}
+
+			return second;
+		} else {
+			return -1;
+		}
+	}
+}
